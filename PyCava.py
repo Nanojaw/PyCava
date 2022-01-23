0885def3397169fb86322d239ce61b415f707dc0
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2022-01-23 10:28:13 +0100

    ran javac

--- PyCava.py
+++ PyCava.py
@@ -1,1 +1,15 @@
 # Tool for generating JNI code
+
+import subprocess
+import os
+import sys
+
+filename = input("File name: ")
+if not os.path.exists(filename + ".java"):
+    print("File does not exist")
+    sys.exit(1)
+
+if not os.path.exists("Cava"): 
+    folder = os.mkdir("Cava")
+
+subprocess.call([os.environ.get("JAVA_HOME") + "/bin/javac.exe", "-d", "./Cava", "-h", "./Cava", filename + ".java"])
---
+++ bridge.java
@@ -0,0 +1,6 @@
+package bridge;
+
+public class bridge {
+    public static native void Log(String message);
+    public static native void Hello();
+}
