5aa96183bab70e1e8c19e69b7a4a9c3a1f141d60
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2022-01-23 10:48:37 +0100

    comments

--- PyCava.py
+++ PyCava.py
@@ -4,12 +4,17 @@ import subprocess
 import os
 import sys
 
-filename = input("File name: ")
-if not os.path.exists(filename + ".java"):
-    print("File does not exist")
-    sys.exit(1)
+# Getting filename
+filename = "bridge"
+#filename = input("File name: ")
+#if not os.path.exists(filename + ".java"):
+#    print("File does not exist")
+#    sys.exit(1)
 
+# Creating Cava folder
 if not os.path.exists("Cava"): 
     folder = os.mkdir("Cava")
 
+# Calling javac with -h flag
 subprocess.call([os.environ.get("JAVA_HOME") + "/bin/javac.exe", "-d", "./Cava", "-h", "./Cava", filename + ".java"])
+
