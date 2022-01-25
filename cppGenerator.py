c689a26273d51d2dce748f90ccd75d8ab2cd1e39
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2022-01-25 17:55:53 +0100

    generator work

--- cppGenerator.py
+++ cppGenerator.py
@@ -1,36 +1,32 @@
 def generate (filename, hFilename, content):
     lineString = f"#include <string>\n#include <{filename}.h>\n#include <{hFilename}>\n"
 
-    # Getting the functions in the header which javac created
-    hFuncs = []
+    #  Adding all of the functions to be wrapped
+    for i in range(len(content.methods)):
 
-    JNIFile = open(hFilename, 'r')
-    JNILines = JNIFile.readlines()
-    for i in range(len(JNILines)):
-        if JNILines[i].startswith("JNIEXPORT"):
-            last = 0
-            line = list(JNILines[i + 1])
-            for j in range(JNILines[i + 1].count(',') + 1):
-                comma = JNILines[i + 1].find(',', last)
-                line.insert(comma, f" v{j}")
-                last = comma + 1
+        # Adding definition to functions in header file
+        defFunc = content.methods[i][:content.methods[i].find(' ') + 1] + "Java_"
+        for pack in content.packages:
+            defFunc += pack + "_"
+        defFunc += content.classs + "_" + content.methods[i][content.methods[i].find(" ") + 1:content.methods[i].find("(")]
 
-            JNILines[i + 1] = "".join(line)
-
-            hFuncs.append(JNILines[i] + " " + JNILines[i + 1])
-
-    for i in range(len(hFuncs)):
+        # Adding parameters to definition
+        defFunc += " (JNIEnv* env, jclass object"
+        
+        
         # Getting cpp function
         cppFunc = ""
         for pack in content.packages:
             cppFunc += pack + "."
-        cppFunc += content.classs + "." + content.methods[i][content.methods[i].find(" ") + 1:]
+        cppFunc += content.classs + "." + content.methods[i][content.methods[i].find(" ") + 1:content.methods[i].find("(")]
+
+        # Adding parameters to cpp function
 
         #TODO Make parameters work
 
         lineString += (
         "\n" + 
-        hFuncs[i][:len(hFuncs[i])-2] + 
+        defFunc + 
         "\n{\n" +
         "    return " + cppFunc + "}\n"
         )
