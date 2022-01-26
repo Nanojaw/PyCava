32b99c9848d0f141341f077dfbee2afd7754aad1
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2022-01-26 16:56:15 +0100

    cppGenerator works now in theory

--- cppGenerator.py
+++ cppGenerator.py
@@ -35,13 +35,38 @@ def generate (filename, hFilename, content):
             cppFunc += pack + "."
         cppFunc += content.classs + "." + content.methods[i][content.methods[i].find(" ") + 1:content.methods[i].find("(")]
 
-        # Adding parameters to cpp function TODO
+        # Adding parameters to cpp function
+
+        reference = "" # Reference and release is for handling reference objects that require cleanup
+        release = ""
+
+        cppFunc += "("
+        for j in range(len(params)):
+            if params[j] == "char" : cppFunc += ", (char)v" + str(j)
+            elif params[j] == "short" : cppFunc += ",  (short)v" + str(j)
+            elif params[j] == "int": cppFunc += ", (int)v" + str(j)
+            elif params[j] == "long": cppFunc += ", (long long)v" + str(j)
+            elif params[j] == "float": cppFunc += ", (float)v" + str(j)
+            elif params[j] == "double": cppFunc += ", (double)v" + str(j)
+            elif params[j] == "wchar_t": cppFunc += ", (wchar_t)v" + str(j)
+            elif params[j] == "bool": cppFunc += ", (bool)v" + str(j)
+            elif params[j] == "std::wstring": 
+                reference += "    const wchar_t* r" + str(j) + " = (*env)->GetStringChars(env, v" + str(j) + ", nullptr);\n"
+                cppFunc += ", std::wstring(r" + str(j) + ")"
+                release += "    (*env)->ReleaseStringChars(env, v" + str(j) + ", r" + str(j) + ");\n"
+        cppFunc += ")\n"
 
         lineString += (
         "\n" + 
         defFunc + 
         "{\n" +
-        "    return " + cppFunc + "}\n"
+        reference +
+        "    auto result = " + cppFunc +
+        release +
+        "    return result;\n"
+        "}\n"
         )
 
-    print(lineString)
+    file = open(filename +  ".cpp", 'w')
+    file.write(lineString)
+    file.close()
